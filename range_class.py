2733cf300f5859de47be23b08a94339da6f9bb9f
Saim Islam <saim.islam698@gmail.com>
2024-06-27 15:23:14 +0600

    Update range_class.py

--- range_class.py
+++ range_class.py
@@ -1,1 +1,20 @@
-
+class Range:
+  def __init__(self, start, stop=None, step=1):
+    #If there is one operator I use no space, e.g: 'step==0'
+    if step==0:
+      raise ValueError("Step Can't be '0'")
+    if stop is None:
+      start, stop = 0, start
+    self._length=max(0,(stop-start+step-1)//step)
+    self._start=start
+    self._step=step
+  def __len__(self):
+    return self._length
+  def __getitem__(self, position):
+    if position<0:
+      position+=len(self)
+    if not (0 <= position < self._length):
+      raise IndexError("Index out of range")
+    #If there is multiple operator I use space, e.g: 'self._start + position * self._step'
+    return self._start + position * self._step
+    
