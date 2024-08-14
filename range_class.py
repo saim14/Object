326ccfde0f1d2a1f9355a28655651d52999959d5
Saim Islam <saim.islam698@gmail.com>
2024-08-14 11:59:38 +0000

    Range Class is created and object is tested

--- range_class.py
+++ range_class.py
@@ -1,20 +1,24 @@
 class Range:
   def __init__(self, start, stop=None, step=1):
-    #If there is one operator I use no space, e.g: 'step==0'
-    if step==0:
-      raise ValueError("Step Can't be '0'")
+    if step == 0:
+      raise ValueError("Step Can't be zero")
+    
     if stop is None:
       start, stop = 0, start
-    self._length=max(0,(stop-start+step-1)//step)
-    self._start=start
-    self._step=step
+
+    self._length = max(0, (stop - start + step - 1)//step)
+    self._start = start 
+    self._step = step 
+
   def __len__(self):
     return self._length
-  def __getitem__(self, position):
-    if position<0:
-      position+=len(self)
-    if not (0 <= position < self._length):
+
+  def __getitem__(self, j):
+    if j < 0:
+      j+=len(self)
+
+    if not(0 <= j < self._length):
       raise IndexError("Index out of range")
-    #If there is multiple operator I use space, e.g: 'self._start + position * self._step'
-    return self._start + position * self._step
+
+    return self._start + j * self._step
     
